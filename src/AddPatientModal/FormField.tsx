dc4e3be7184d6cc0d17e941057bf329247a29d1a
Matti Luukkainen <mluukkai@iki.fi>
2020-03-11 13:42:21 +0200

    Update FormField.tsx

--- src/AddPatientModal/FormField.tsx
+++ src/AddPatientModal/FormField.tsx
@@ -51,3 +51,39 @@ export const TextField: React.FC<InputProps & {
     {errorMessage && <Label color="red">{errorMessage}</Label>}
   </Form.Field>
 );
+
+/*
+  for exercises 9.24.-
+*/
+export const ArrayField: React.FC<InputProps & {
+  label: string;
+  errorMessage?: boolean | string;
+}> = ({ diagnosisCodes, label, placeholder, errorMessage }) => { 
+  const [code, setCode] = React.useState('M24.2')
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) =>
+    setCode(event.target.value)
+
+  const onClick = () => {
+    if (code.length>0) {
+      diagnosisCodes.push(code)
+      setCode('')
+    }
+  }
+  
+  return(
+    <Form.Field>
+      <label>{label}</label>
+      {diagnosisCodes.length>0&&
+        <Segment>
+          <em>{diagnosisCodes.join(', ')}</em>
+        </Segment>
+      }
+      <Input value={code} onChange={onChange} placeholder={placeholder} />
+      <Button type="button" onClick={onClick}>
+        add
+      </Button>
+      {errorMessage && <Label color="red">{errorMessage}</Label>}
+    </Form.Field>
+  )
+};
