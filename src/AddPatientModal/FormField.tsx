0f0acaaa6a219b19f2c163f5385d94a029e808cc
Huy Bui <huythdnbkltv@gmail.com>
2022-06-25 19:47:42 +0300

    fix: missing last selected diagnosis code
    Calling setDiagnoses does not change selectedDiagnoses immediately. As a result, the last selected diagnosis code never appears on the field value. Use value of the select field directly instead.

--- src/AddPatientModal/FormField.tsx
+++ src/AddPatientModal/FormField.tsx
@@ -114,7 +114,7 @@ export const DiagnosisSelection = ({
   const onChange = (data: string[]) => {    
     setDiagnoses([...data]);
     setFieldTouched(field, true);
-    setFieldValue(field, selectedDiagnoses);
+    setFieldValue(field, [...data]);
   };
 
   const stateOptions = diagnoses.map((diagnosis) => ({
