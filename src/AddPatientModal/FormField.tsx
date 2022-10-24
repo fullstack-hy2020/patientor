08d64767eec1f90c99cc30ece403908e55d2ce91
Abdirahman Suldaan <suldaan.89@hotmail.com>
2022-10-24 17:28:47 +0300

    fixed the bug diagnosis codes

--- src/AddPatientModal/FormField.tsx
+++ src/AddPatientModal/FormField.tsx
@@ -1,14 +1,14 @@
-import React, { useState } from "react";
-import { ErrorMessage, Field, FieldProps, FormikProps } from "formik";
+import React, { useState } from 'react';
+import { ErrorMessage, Field, FieldProps, FormikProps } from 'formik';
 import {
   Select,
   FormControl,
   MenuItem,
   TextField as TextFieldMUI,
   Typography,
-} from "@material-ui/core";
-import { Diagnosis, Gender } from "../types";
-import { InputLabel } from "@material-ui/core";
+} from '@material-ui/core';
+import { Diagnosis, Gender } from '../types';
+import { InputLabel } from '@material-ui/core';
 import Input from '@material-ui/core/Input';
 
 // structure of a single option
@@ -24,14 +24,16 @@ type SelectFieldProps = {
   options: GenderOption[];
 };
 
-const FormikSelect = ({ field, ...props }: FieldProps) => <Select {...field} {...props} />;
+const FormikSelect = ({ field, ...props }: FieldProps) => (
+  <Select {...field} {...props} />
+);
 
 export const SelectField = ({ name, label, options }: SelectFieldProps) => (
   <>
     <InputLabel>{label}</InputLabel>
     <Field
       fullWidth
-      style={{ marginBottom: "0.5em" }}
+      style={{ marginBottom: '0.5em' }}
       label={label}
       component={FormikSelect}
       name={name}
@@ -51,14 +53,14 @@ interface TextProps extends FieldProps {
 }
 
 export const TextField = ({ field, label, placeholder }: TextProps) => (
-  <div style={{ marginBottom: "1em" }}>
+  <div style={{ marginBottom: '1em' }}>
     <TextFieldMUI
       fullWidth
       label={label}
       placeholder={placeholder}
       {...field}
     />
-    <Typography variant="subtitle2" style={{ color: "red" }}>
+    <Typography variant="subtitle2" style={{ color: 'red' }}>
       <ErrorMessage name={field.name} />
     </Typography>
   </div>
@@ -77,7 +79,7 @@ export const NumberField = ({ field, label, min, max }: NumberProps) => {
   const [value, setValue] = useState<number>();
 
   return (
-    <div style={{ marginBottom: "1em" }}>
+    <div style={{ marginBottom: '1em' }}>
       <TextFieldMUI
         fullWidth
         label={label}
@@ -91,9 +93,9 @@ export const NumberField = ({ field, label, min, max }: NumberProps) => {
           if (value > max) setValue(max);
           else if (value <= min) setValue(min);
           else setValue(Math.floor(value));
-      }}
+        }}
       />
-      <Typography variant="subtitle2" style={{ color: "red" }}>
+      <Typography variant="subtitle2" style={{ color: 'red' }}>
         <ErrorMessage name={field.name} />
       </Typography>
     </div>
@@ -106,15 +108,15 @@ export const DiagnosisSelection = ({
   setFieldTouched,
 }: {
   diagnoses: Diagnosis[];
-  setFieldValue: FormikProps<{ diagnosisCodes: string[] }>["setFieldValue"];
-  setFieldTouched: FormikProps<{ diagnosisCodes: string[] }>["setFieldTouched"];
+  setFieldValue: FormikProps<{ diagnosisCodes: string[] }>['setFieldValue'];
+  setFieldTouched: FormikProps<{ diagnosisCodes: string[] }>['setFieldTouched'];
 }) => {
   const [selectedDiagnoses, setDiagnoses] = useState<string[]>([]);
-  const field = "diagnosisCodes";
-  const onChange = (data: string[]) => {    
+  const field = 'diagnosisCodes';
+  const onChange = (data: string[]) => {
     setDiagnoses([...data]);
     setFieldTouched(field, true);
-    setFieldValue(field, selectedDiagnoses);
+    setFieldValue(field, [...data]);
   };
 
   const stateOptions = diagnoses.map((diagnosis) => ({
@@ -126,7 +128,12 @@ export const DiagnosisSelection = ({
   return (
     <FormControl style={{ width: 552, marginBottom: '30px' }}>
       <InputLabel>Diagnoses</InputLabel>
-      <Select multiple value={selectedDiagnoses} onChange={(e) => onChange(e.target.value as string[])} input={<Input />}>
+      <Select
+        multiple
+        value={selectedDiagnoses}
+        onChange={(e) => onChange(e.target.value as string[])}
+        input={<Input />}
+      >
         {stateOptions.map((option) => (
           <MenuItem key={option.key} value={option.value}>
             {option.text}
